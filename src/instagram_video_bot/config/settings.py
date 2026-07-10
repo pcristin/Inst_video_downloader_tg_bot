be8094ee9b0b916654ee479fd11ed3046c9088d8
root <root@vm3554026.stark-industries.solutions>
2026-07-10 11:14:29 +0000

    fix: allow empty optional environment values

--- src/instagram_video_bot/config/settings.py
+++ src/instagram_video_bot/config/settings.py
@@ -118,7 +118,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=True
+        case_sensitive=True,
+        env_ignore_empty=True,
     )
     
     def __init__(self, **kwargs):
